90b0edce6f8229e61c689eeb58ca5ba2619d1b2e
mass10 <mass10.github@gmail.com>
2018-12-08 18:41:21 +0900

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 virtualenv .
 source bin/activate
 pip3 install -r requirements.txt
-python3 main.py
+python3 main.py address '0.0.0.0'
 deactivate
 
--- main.py
+++ main.py
@@ -1,14 +1,35 @@
 #!/usr/bin/env python3
 # coding: utf-8
+#
+#
+#
+#
+#   pip3 install responder
+#
+#
+#
+#
+
+import sys
 import responder
 
 api = responder.API()
+
+#
+# routings:
+#   上から順に適用されます。
+#
+
+@api.route("/hello/{name}")
+async def greet_world(req, res, *, name):
+	res.text = f"welcome, {name}."
 
 @api.route("/{greeting}")
 async def greet_world(req, res, *, greeting):
 	res.text = f"{greeting}, world!"
 
-def main():
-	api.run()
+def main(argv):
+	argv = argv[1:]
+	api.run(address="0.0.0.0", port=8080)
 
-main()
+main(sys.argv)
